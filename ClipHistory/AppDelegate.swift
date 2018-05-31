3fdd92fe54b3200f082d289fb24cdb3d75767646
Paul Klym <paul_klym@mail.ru>
2018-05-31 18:19:26 +0300

    Initial Commit

---
+++ ClipHistory/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ClipHistory
+//
+//  Created by paul on 5/31/18.
+//  Copyright © 2018 Paul Nameless. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ClipHistory/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ClipHistory
+//
+//  Created by paul on 5/31/18.
+//  Copyright © 2018 Paul Nameless. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
